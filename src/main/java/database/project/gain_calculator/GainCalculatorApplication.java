f4de5b49c1b5e32ac76d4ceffeb054ea187df588
inan <ozsahinan@gmail.com>
2024-10-26 17:49:18 +0200

    Initial commit

---
+++ src/main/java/database/project/gain_calculator/GainCalculatorApplication.java
@@ -0,0 +1,13 @@
+package database.project.gain_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GainCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GainCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/database/project/gain_calculator/GainCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package database.project.gain_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GainCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
